7b77c2e565463875196ab358b39ac57b0bd4bc02
John Beckhoff <john.beckhoff@outlook.com>
2019-11-04 19:21:25 -0600

    Initial Commit

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ActionviewDeepDive
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    	config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.stylesheets     true
+      g.javascripts     true
+      g.scaffold_stylesheet false
+    end
+
+# Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
